94e5c7bfd47409dadbbf3b6715bc69111c3edf7c
waiyanmyatoo <57125160+waiyanmyatoo@users.noreply.github.com>
2023-12-26 19:11:51 +0630

    Implement Simple Form Login

---
+++ src/main/java/com/example/securityformlogin/SecurityFormLoginApplication.java
@@ -0,0 +1,12 @@
+package com.example.securityformlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityFormLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityFormLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securityformlogin/SecurityFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityformlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFormLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
